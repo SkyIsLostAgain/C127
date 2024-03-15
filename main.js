41d2d9fdeb4bfb9e6068114db64b2c8a8d264882
SkyIsLostAgain <136880731+SkyIsLostAgain@users.noreply.github.com>
2024-03-15 20:54:59 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -3,19 +3,34 @@ song = "";
 function preload(){
     song = loadSound("music.mp3");
 }
+
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNte.on('pose', gotPoses);
 }
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized.')
+}
+
+function gotPoses(result){
+    if(result.length > 0){
+        console.log(results);
+    }
+}
+
 function draw(){
     image(video, 0, 0, 300, 300)
 }
 
 function play(){
     song.play();
-    song.setVolume(0.7);
+    song.setVolume(0.5);
     song.rate(0.5);
 }
