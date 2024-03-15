7f5f9510eec0dbe98be6d8377553564e46c129ec
SkyIsLostAgain <136880731+SkyIsLostAgain@users.noreply.github.com>
2024-03-15 21:09:21 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ song = "";
 leftWristX = 0;
 leftWristY = 0;
 
+rightWristX = 0;
+rightWristY = 0;
+
 function preload(){
     song = loadSound("music.mp3");
 }
@@ -26,7 +29,11 @@ function gotPoses(results){
         console.log(results);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
-        console.log('leftWristX = ' + leftWristX + " leftWristY = " + leftWristY)
+        console.log('leftWristX = ' + leftWristX + " leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log('rightWristX = ' + rightWristX + " rightWristY = " + rightWristY)
     }
 }
 
